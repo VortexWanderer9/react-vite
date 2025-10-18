41d648f26a912ce894a778dc3d46bc01ed48e762
Tyrell Wellick <keshavniure99@gmail.com>
2025-10-18 04:27:48 -0800

    create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import Search from './assets/Search'
 
 function App() {
 return (
    <>
-   <h1>Hello world</h1>
+  <section className='hero'>
+    <div className="background-image">
+    </div>
+ <header className='flex flex-col items-center justify-center gap-4 text-center'>
+  <img className='w-20 h-20' src="./logo.png" alt="" />
+  <img className='relative z-10 mt-4' src="./hero.png" alt="" />
+  <h1 className="text-4xl md:text-6xl font-semibold text-gray-100 italic mt-4">
+    “Every great <span className='text-blue-900'>story</span> begins with a search.”
+  </h1>
+  <Search />
+</header>
+
+  </section>
    </>
   )
 
---
+++ src/assets/Search.jsx
@@ -0,0 +1,12 @@
+const Search = () => {
+  return (
+    <div className="search">
+      <img className="" src="./search.svg" alt="" />
+     <input className="border-none outline-none" type="text" placeholder="Search Your favorite Movies" />
+    </div>
+  )
+}
+
+
+
+export default Search;
